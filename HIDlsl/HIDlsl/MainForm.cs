5a1ddf8b0e3bde82f2144f29f686fdfb68416467
Mark M. Span <m.m.span@rug.nl>
2025-04-21 17:28:55 +0200

    First Re-iteration.

--- HIDlsl/HIDlsl/MainForm.cs
+++ HIDlsl/HIDlsl/MainForm.cs
@@ -1,77 +1,124 @@
-
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+using HidLibrary;
 using LSL;
-using SharpDX.DirectInput;
-
-
-
-/*
- * To read the XDF data correctly into MATLAB use load_xdf, and then do:
- * 
- * plot(double(bitshift(uint32(data'), -8)) / 2.7). 
- * 
- * The resulting data should be in kg then (approximately)
- */
 
 namespace HIDlsl
 {
     public partial class MainForm : Form
     {
-        volatile static Boolean Linked = false;
-        Thread? LSLThread;
-        Joystick? joystick;
-        readonly DirectInput? directInput = new();
-        readonly List<DeviceInstance> deviceList = new();
-        DeviceInstance? device;
-
+        private volatile static bool Linked = false;
+        private Thread? LSLThread;
+        private HidDevice? hidDevice;
+        private readonly List<HidDevice> deviceList = new();
+        private HidDevice? device;
+        private liblsl.StreamOutlet? outlet;
+        private List<CheckBox> checkBoxes = new();
+        private Dictionary<string, int> inputReportOffsets = new();
 
         public MainForm()
-
         {
             InitializeComponent();
+            InitializeDeviceList();
+            InitializeUI();
+        }
 
-            // Find a Joystick Guid (for BB)
+        private void InitializeUI()
+        {
+            BoardSelector.SelectedIndexChanged += BoardSelector_SelectedIndexChanged;
+            LinkButton.Click += LinkButton_Click;
+        }
 
-            foreach (var deviceInstance in directInput.GetDevices(DeviceType.Joystick, DeviceEnumerationFlags.AllDevices))
-                if (deviceInstance.ProductName.Contains("Adapted BalanceBoard"))  // Guid for the Adapted BalanceBoard
-                {
-                    this.BoardSelector.Items.Add(deviceInstance.ProductName);
-                    deviceList.Add(deviceInstance);
-                    this.BoardSelector.SelectedIndex++;
-                }
+        private void BoardSelector_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Clear existing checkboxes
+            Controls.Clear();
+            checkBoxes.Clear();
+            inputReportOffsets.Clear();
 
+            // Get the selected device
+            device = deviceList.ElementAt(BoardSelector.SelectedIndex);
 
-            // or an Supplemental, I dont know why some arduino micros advertise themselves as 'Supplemental'
-            foreach (var deviceInstance in directInput.GetDevices(DeviceType.Supplemental, DeviceEnumerationFlags.AllDevices))
-                if (deviceInstance.ProductName.Contains("Adapted BalanceBoard"))  // Guid for the Adapted BalanceBoard
-                {
-                    this.BoardSelector.Items.Add(deviceInstance.ProductName);
-                    deviceList.Add(deviceInstance);
-                    this.BoardSelector.SelectedIndex++;
-                }
+            // Enumerate input reports
+            var capabilities = device.Capabilities;
+            int top = 50;
+            int offset = 0;
 
-            // If Joystick not found, throws an error
-            if (deviceList.Count < 1)
+            // Assuming each input report is 4 bytes (int32)
+            int reportLength = capabilities.InputReportByteLength;
+            int reportCount = reportLength / 4;
+
+            for (int i = 0; i < reportCount; i++)
             {
-                this.LinkButton.Text = "No Board Attached!";
-                this.LinkButton.Enabled = false;
-                this.BackColor = Color.Pink;
+                var checkBox = new CheckBox
+                {
+                    Text = $"Input Report {i}",
+                    Top = top,
+                    Left = 10
+                };
+                checkBoxes.Add(checkBox);
+                inputReportOffsets[checkBox.Text] = offset;
+                Controls.Add(checkBox);
+                top += 20;
+                offset += 4;
             }
         }
 
-        void StartLSL(Button sender)
+        private string? ExtractDeviceName(string devicePath)
         {
-            if (Linked == false)
+            int lastIndex = devicePath.LastIndexOf('\\');
+            if (lastIndex >= 0 && lastIndex < devicePath.Length - 1)
+            {
+                return devicePath.Substring(lastIndex + 1);
+            }
+            return null;
+        }
+
+        private void InitializeDeviceList()
+        {
+            var devices = HidDevices.Enumerate();
+
+            foreach (var device in devices)
+            {
+                if (ExtractDeviceName(device.DevicePath).Contains("Adapted BalanceBoard"))
+                {
+                    AddDeviceToList(device);
+                }
+            }
+
+            if (deviceList.Count < 1)
+            {
+                DisplayNoDeviceError();
+            }
+        }
+
+        private void AddDeviceToList(HidDevice device)
+        {
+            BoardSelector.Items.Add(ExtractDeviceName(device.DevicePath));
+            deviceList.Add(device);
+            BoardSelector.SelectedIndex = 0;
+        }
+
+        private void DisplayNoDeviceError()
+        {
+            LinkButton.Text = "No Board Attached!";
+            LinkButton.Enabled = false;
+            BackColor = Color.Pink;
+        }
+
+        private void StartLSL(Button sender)
+        {
+            if (!Linked)
             {
                 Linked = true;
                 sender.Text = "Unlink";
                 BoardSelector.Enabled = false;
                 device = deviceList.ElementAt(BoardSelector.SelectedIndex);
-                var id = device.ProductName;
-                if (id.Contains("BalanceBoard"))
-                    LSLThread = new Thread(() => MainForBB(Board: device.InstanceGuid));
-                else if (id.Contains("Micro"))
-                    LSLThread = new Thread(() => MainForRE(Board: device.InstanceGuid));
-
+                LSLThread = new Thread(() => MainForBB(device));
                 LSLThread?.Start();
             }
             else
@@ -79,168 +126,67 @@ namespace HIDlsl
                 Linked = false;
                 BoardSelector.Enabled = true;
                 sender.Text = "Link";
-                // give the thread time to stop broadcasting.
                 Thread.Sleep(1000);
                 LSLThread = null;
             }
         }
-        private void MainForRE(Guid Board)
+
+        private void MainForBB(HidDevice device)
         {
-            joystick = new Joystick(directInput, Board);
+            device.OpenDevice();
+            device.MonitorDeviceEvents = true;
+            device.ReadReport(OnReadReport);
 
-            // Set BufferSize in order to use buffered data.
-            joystick.Properties.BufferSize = 20;
+            var selectedChannels = checkBoxes.Where(cb => cb.Checked).Select(cb => cb.Text).ToList();
+            var info = new liblsl.StreamInfo(ExtractDeviceName(device.DevicePath) + "(USB)", "Mocap", selectedChannels.Count, 100, liblsl.channel_format_t.cf_int32, "sddsfsdf");
+            var setup = info.desc().append_child("setup");
+            setup.append_child_value("Author", "M.M.Span");
+            setup.append_child_value("Manufacturer", "University of Groningen");
+            setup.append_child_value("Manual", "markspan.github.io");
+            setup.append_child_value("Model", "Adapted USB WiiBalanceBoard");
 
-            // Acquire the joystick
-            joystick.Acquire();
-
-            // Initialize LSL:
-            liblsl.StreamInfo info = new(device?.ProductName + "(USB)", "Mocap", 2, 100, liblsl.channel_format_t.cf_int32, "sddsfsdf");
-            liblsl.XMLElement Setup = info.desc().append_child("setup");
-
-            Setup.append_child_value("Author", "M.M.Span");
-            Setup.append_child_value("Manifacturer", "University of Groningen");
-            Setup.append_child_value("Manual", "markspan.github.io");
-            Setup.append_child_value("Model", "Rotary ForceFeed");
-
-            liblsl.XMLElement Channels = info.desc().append_child("channels");
-            Channels.append_child("channel")
-                .append_child_value("label", "Rotary Position")
-                .append_child_value("type", "Force")
-                .append_child_value("unit", "Arbitrary units");
-            Channels.append_child("channel")
-                .append_child_value("label", "SawTooth")
-                .append_child_value("type", "Calibration")
-                .append_child_value("unit", "Arbitrary units");
-
-            liblsl.StreamOutlet outlet = new(info);
-
-            int[] lslout = new int[2];
-            bool newdata = false;
-            int saw = 0;
-            // Poll events from joystick
-            while (Linked == true)
+            var channels = info.desc().append_child("channels");
+            foreach (var channel in selectedChannels)
             {
-                while (newdata == false)
-                {
-                    var datas = joystick.GetBufferedData();
-                    foreach (var state in datas)
-                    {
-                        switch (state.Offset)
-                        {
-                            case JoystickOffset.X:
-                                // Rotary encoder
-                                lslout[0] = state.Value;
-                                break;
-                            case JoystickOffset.RotationY:
-                                // RotationY axis is the sawtooth axis. If this value changes a new sample is recorded.
-                                // This method anables sampling with a fixed sample frequency: the sample frequency of the Encoder.
-                                lslout[1] = ++saw;
-                                if (saw >= 100)
-                                    saw = 0;
-                                newdata = true;
-                                break;
-                            default:
-                                break;
-                        }
-                    }
-                }
-                outlet.push_sample(lslout);
-                newdata = false;
+                AddChannel(channels, channel, "Force", "kilograms");
+            }
+
+            outlet = new liblsl.StreamOutlet(info);
+
+            while (Linked)
+            {
+                device?.ReadReport(OnReadReport);
             }
             System.GC.Collect();
+            device?.CloseDevice();
         }
 
-        private void MainForBB(Guid Board)
+        private static void AddChannel(liblsl.XMLElement channels, string label, string type, string unit)
         {
+            channels.append_child("channel")
+                .append_child_value("label", label)
+                .append_child_value("type", type)
+                .append_child_value("unit", unit);
+        }
 
-            joystick = new Joystick(directInput, Board);
+        private void OnReadReport(HidReport report)
+        {
+            if (report == null) return;
 
-            // Set BufferSize in order to use buffered data.
-            joystick.Properties.BufferSize = 20;
+            var data = report.Data;
+            var lslout = new int[checkBoxes.Count(cb => cb.Checked)];
+            int index = 0;
 
-            // Acquire the joystick
-            joystick.Acquire();
-
-            // Initialize LSL:
-            liblsl.StreamInfo info = new(device?.ProductName + "(USB)", "Mocap", 5, 100, liblsl.channel_format_t.cf_int32, "sddsfsdf");
-            liblsl.XMLElement Setup = info.desc().append_child("setup");
-
-            Setup.append_child_value("Author", "M.M.Span");
-            Setup.append_child_value("Manifacturer", "University of Groningen");
-            Setup.append_child_value("Manual", "markspan.github.io");
-            Setup.append_child_value("Model", "Adapted USB WiiBalanceBoard");
-
-            liblsl.XMLElement Channels = info.desc().append_child("channels");
-            Channels.append_child("channel")
-                .append_child_value("label", "Weight_BottomLeft")
-                .append_child_value("type", "Force")
-                .append_child_value("unit", "kilograms");
-            Channels.append_child("channel")
-                .append_child_value("label", "Weight_TopLeft")
-                .append_child_value("type", "Force")
-                .append_child_value("unit", "kilograms");
-            Channels.append_child("channel")
-                .append_child_value("label", "Weight_TopRight")
-                .append_child_value("type", "Force")
-                .append_child_value("unit", "kilograms");
-            Channels.append_child("channel")
-                .append_child_value("label", "Weight_BottomRight")
-                .append_child_value("type", "Force")
-                .append_child_value("unit", "kilograms");
-            Channels.append_child("channel")
-                .append_child_value("label", "SawTooth")
-                .append_child_value("type", "Calibration")
-                .append_child_value("unit", "Arbitrary units");
-
-            liblsl.StreamOutlet outlet = new(info);
-
-            int[] lslout = new int[5];
-            bool newdata = false;
-            int saw = 0;
-            // Poll events from joystick
-            while (Linked == true)
+            foreach (var checkBox in checkBoxes)
             {
-                while (newdata == false)
+                if (checkBox.Checked)
                 {
-                    var datas = joystick.GetBufferedData();
-                    foreach (var state in datas)
-                    {
-                        switch (state.Offset)
-                        {
-                            case JoystickOffset.X:
-                                // Left Bottom Sensor
-                                lslout[0] = state.Value;
-                                break;
-                            case JoystickOffset.Y:
-                                // Left Top Sensor
-                                lslout[1] = state.Value;
-                                break;
-                            case JoystickOffset.Z:
-                                // Right Top Sensor
-                                lslout[2] = state.Value;
-                                break;
-                            case JoystickOffset.RotationX:
-                                // Right Bottom Sensor
-                                lslout[3] = state.Value;
-                                break;
-                            case JoystickOffset.RotationY:
-                                // RotationY axis is the sawtooth axis. If this value changes a new sample is recorded.
-                                // This method anables sampling with a fixed sample frequency: the sample frequency of the BalanceBoard.
-                                lslout[4] = ++saw;
-                                if (saw >= 100)
-                                    saw = 0;
-                                newdata = true;
-                                break;
-                            default:
-                                break;
-                        }
-                    }
+                    int offset = inputReportOffsets[checkBox.Text];
+                    lslout[index++] = BitConverter.ToInt32(data, offset);
                 }
-                outlet.push_sample(lslout);
-                newdata = false;
             }
-            System.GC.Collect();
+
+            outlet?.push_sample(lslout);
         }
 
         private void LinkButton_Click(object sender, EventArgs e)
